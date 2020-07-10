40cf373a3fe8070d49833fffc739e00364aea080
SirIsaacNeutron <dbadir@cox.net>
2020-07-10 12:44:08 -0700

    Added Bootstrap and Death Increase Chart
    I also am trying out Recharts instead of Victory to display the data.

--- src/components/StateInfo.js
+++ src/components/StateInfo.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
+import axios from 'axios';
+import { AreaChart, XAxis, YAxis, Area, Tooltip } from 'recharts';
+
 class StateInfo extends React.Component {
     constructor(props) {
         super(props);
@@ -9,12 +12,41 @@ class StateInfo extends React.Component {
         }
     }
 
+    componentDidMount() {
+        const { state } = this.props;
+        axios.get(`https://covidtracking.com/api/v1/states/${state}/daily.json`)
+        .then(resp => this.setState({
+            covidData: resp.data
+        }))
+        .catch(err => console.log(err));
+    }
+
     render() {
+        const deathsConfirmed = this.state.covidData.map(
+            day => ({ 
+                deathsConfirmed: day.deathIncrease,
+                date:  new Date(
+                    day.date.toString().substring(0, 4),
+                    (day.date - 100).toString().substring(4, 6),
+                    day.date.toString().substring(6)
+                )
+            }))
+            .reverse();
+        console.log(`Results for ${this.props.state}`);
+        console.log(deathsConfirmed);
         return (
-            <div className="state-info">
-                <h3>{this.props.stateName}</h3>
+            <div className='state-info'>
+                <h2>{this.props.stateName}</h2>
                 <hr />
-                <p>Charts for {this.props.stateName} go here.</p>
+                <div className='charts'>
+                    <h3>Daily Deaths</h3>
+                    <AreaChart width={350} height={350} data={deathsConfirmed}>
+                        <XAxis dataKey='date' scale='auto' />
+                        <YAxis />
+                        <Area dataKey='deathsConfirmed' fill='black' />
+                        <Tooltip />
+                    </AreaChart>
+                </div>
             </div>
         );
     }
