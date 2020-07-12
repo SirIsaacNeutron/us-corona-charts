b3db8c93800a71ae43a3e2130a00c70a334c4130
SirIsaacNeutron <dbadir@cox.net>
2020-07-12 15:39:15 -0700

    Added custom tooltip
    It's not the best-looking but it gets the job done decently.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,7 +3,29 @@ import PropTypes from 'prop-types';
 
 import { AreaChart, XAxis, YAxis, Area, Tooltip } from 'recharts';
 
-const Chart = (props) => {
+const MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+];
+
+const tooltip = props => {
+    // Based off http://recharts.org/en-US/examples/CustomContentOfTooltip
+    // BOTH of these 'undefined' checks are necessary. Otherwise, the app will
+    // crash if you try to get the tooltip of a different graph. 
+    if (props.active && props.payload !== undefined && props.payload[0] !== undefined) {
+        const d = props.payload[0].payload.date;
+        const dateString = `${MONTH_NAMES[d.getMonth()]} ${d.getDate()}`;
+        return (
+            <div>
+                <p className='mb-0'><small>{dateString}</small></p>
+                { /* .value makes this tooltip reusable with different data */ }
+                <p>{props.payload[0].value}</p>
+            </div>
+        );
+    }
+    return null;
+}
+
+const Chart = props => {
     return (
         <div className='chart'>
             <h3>{props.title}</h3>
@@ -11,7 +33,7 @@ const Chart = (props) => {
                 <XAxis dataKey='date' scale='auto' />
                 <YAxis />
                 <Area dataKey={props.dataKey} fill={props.fill} stroke='none' />
-                <Tooltip />
+                <Tooltip content={tooltip}/>
             </AreaChart>
         </div>
     );
